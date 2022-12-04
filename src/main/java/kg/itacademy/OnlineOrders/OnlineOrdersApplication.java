541a397a509f5b0d50ce2aa9014c52279bc06183
myliz1118 <kalizatilekovna@gmail.com>
2022-12-05 00:03:56 +0600

    Initial commit

---
+++ src/main/java/kg/itacademy/OnlineOrders/OnlineOrdersApplication.java
@@ -0,0 +1,13 @@
+package kg.itacademy.OnlineOrders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/itacademy/OnlineOrders/OnlineOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.itacademy.OnlineOrders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
